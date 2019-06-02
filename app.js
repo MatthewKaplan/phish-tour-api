4865d1f7ac09ba24e662874e74d87c4fb7f85572
Matthew Kaplan <kaplan.matthew.p@gmail.com>
2019-06-01 19:29:23 -0600

    make port dynamic

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
+const PORT = process.env.PORT || 3000;
 const app = express()
-const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/phishTour', (req, res) => res.send('Hello Phish!'))
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(PORT, () => console.log(`App running!`))
