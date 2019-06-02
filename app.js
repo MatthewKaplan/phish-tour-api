55a69fcc84c318d47eeda1b37d589506b76ef40e
Matthew Kaplan <kaplan.matthew.p@gmail.com>
2019-06-01 20:50:35 -0600

    Add read all and read 1 routes

--- app.js
+++ app.js
@@ -1,7 +1,25 @@
 const express = require('express')
+const morgan = require("morgan");
+const dbConnection = require('./connection')
 const PORT = process.env.PORT || 3000;
 const app = express()
 
-app.get('/api/v1/phishTour', (req, res) => res.send('Hello Phish!'))
+app.use(morgan(process.env.NODE_ENV !== "production" ? "dev" : "combined"));
 
-app.listen(PORT, () => console.log(`App running!`))
+app.get('/api/v1/phishTour', (req, res) => {
+  dbConnection("phishTour")
+    .select("*")
+    .then(tour => res.status(200).json(tour))
+    .catch(error => res.status(500).json({ error: error.message, stack: error.stack }));
+});
+
+app.get("/api/v1/phishTour/:id", (req, res) => {
+  dbConnection("phishTour")
+    .select("*")
+    .limit(1)
+    .where({ id: req.params.id })
+    .then(tour => res.status(200).json(tour))
+    .catch(error => res.status(500).json({ error: error.message, stack: error.stack }));
+});
+
+app.listen(PORT, () => console.log(`App running!`));
